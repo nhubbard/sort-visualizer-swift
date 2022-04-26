822ea6d4e548dc0b340e547a62c5fc1ad3e1f606
Nicholas Hubbard <hubbardn95@gmail.com>
2022-04-26 12:44:58 -0400

    Initial Commit

---
+++ Shared/Sort2App.swift
@@ -0,0 +1,17 @@
+//
+//  Sort2App.swift
+//  Shared
+//
+//  Created by Nicholas Hubbard on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Sort2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
