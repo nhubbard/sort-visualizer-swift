c1cfd7e35b89e81cc734bbbfde8f17bc50449006
Nicholas Hubbard <hubbardn95@gmail.com>
2022-06-08 10:13:19 -0400

    Insertion Sort: Implement Ruby version

--- Shared/Resources/insertionsort.bundle/insertionsort.rb
+++ Shared/Resources/insertionsort.bundle/insertionsort.rb
@@ -1,5 +1,18 @@
-# TODO: Insert Ruby implementation of algorithm here.
+def insertionSort(arr)
+  n = arr.length()
+  i = 1
+  while i < n
+    key = arr[i]
+    j = i - 1
+    while j >= 0 && arr[j] > key
+      arr[j + 1] = arr[j]
+      j -= 1
+    end
+    arr[j + 1] = key
+    i += 1
+  end
+end
 
 items = [35, 95, 74, 71, 72, 30, 96, 53, 9, 0]
-sort(items)
+insertionSort(items)
 p items
