a08449519054265412523dae3f85cee04def33f1
Nicholas Hubbard <hubbardn95@gmail.com>
2022-06-08 10:08:05 -0400

    Insertion Sort: Implement Python version

--- Shared/Resources/insertionsort.bundle/insertionsort.py
+++ Shared/Resources/insertionsort.bundle/insertionsort.py
@@ -1,7 +1,14 @@
-# TODO: Insert Python implementation of algorithm here.
+def insertionSort(arr):
+  n = len(arr)
+  for i in range(1, n):
+    key = arr[i]
+    j = i - 1
+    while j >= 0 and arr[j] > key:
+      arr[j + 1] = arr[j]
+      j = j - 1
+    arr[j + 1] = key
 
 if __name__ == "__main__":
   array = [35, 95, 74, 71, 72, 30, 96, 53, 9, 0]
-  # TODO: Uncomment when finished to satisfy Pyright.
-  # sort(array)
+  insertionSort(array)
   print(array)
