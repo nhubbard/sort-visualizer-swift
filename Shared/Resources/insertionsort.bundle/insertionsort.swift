1adeaf2899b45568a9c41094425a59cac8bd9f09
Nicholas Hubbard <hubbardn95@gmail.com>
2022-06-08 10:15:22 -0400

    Insertion sort: finish Swift implementation

--- Shared/Resources/insertionsort.bundle/insertionsort.swift
+++ Shared/Resources/insertionsort.bundle/insertionsort.swift
@@ -1,5 +1,16 @@
-// TODO: Insert Swift implementation of algorithm here.
+func insertionSort(_ arr: inout [Int]) {
+  let n = arr.count
+  for i in 1..<n {
+    let key = arr[i]
+    var j = i - 1
+    while j >= 0 && arr[j] > key {
+      arr[j + 1] = arr[j]
+      j = j - 1
+    }
+    arr[j + 1] = key
+  }
+}
 
 var items: [Int] = [35, 95, 74, 71, 72, 30, 96, 53, 9, 0]
-sort(&items)
+insertionSort(&items)
 print(items)
