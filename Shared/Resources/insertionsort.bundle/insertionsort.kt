cb39b367e80b4d6aba8dfe0fa4d773e8f9fa90c9
Nicholas Hubbard <hubbardn95@gmail.com>
2022-06-08 09:59:16 -0400

    Insertion Sort: Implement Kotlin version

--- Shared/Resources/insertionsort.bundle/insertionsort.kt
+++ Shared/Resources/insertionsort.bundle/insertionsort.kt
@@ -1,7 +1,18 @@
-/* TODO: Insert Kotlin implementation of algorithm here. */
+fun insertionSort(arr: Array<Int>) {
+  val n = arr.size
+  for (i in 1 until n) {
+    val key = arr[i]
+    var j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j]
+      j = j - 1
+    }
+    arr[j + 1] = key
+  }
+}
 
 fun main() {
   var items = arrayOf<Int>(35, 95, 74, 71, 72, 30, 96, 53, 9, 0)
-  sort(items)
+  insertionSort(items)
   println("[%s]".format(items.joinToString(", ")))
 }
