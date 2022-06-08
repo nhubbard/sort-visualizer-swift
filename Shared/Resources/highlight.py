155df75521dbbf516a766429c19c91a1293a1d73
Nicholas Hubbard <hubbardn95@gmail.com>
2022-06-08 10:17:11 -0400

    Update highlight.py format and add insertion sort to list

--- Shared/Resources/highlight.py
+++ Shared/Resources/highlight.py
@@ -4,33 +4,33 @@ from pygments import highlight
 from pygments.lexers import get_lexer_for_filename
 from pygments.formatter import Formatter
 
-algorithms = ["quicksort", "mergesort", "heapsort", "bubblesort", "selectionsort"]
+algorithms = ["quicksort", "mergesort", "heapsort", "bubblesort", "selectionsort", "insertionsort"]
 extensions = ["c", "cpp", "cs", "go", "java", "js", "kt", "py", "rb", "swift"]
 
 class JsonFormatter(Formatter):
-    def __init__(self):
-        self.tokens = []
+  def __init__(self):
+    self.tokens = []
 
-    def format(self, tokensource, _):
-        for ttype, value in tokensource:
-            self.tokens.append({"type": str(ttype), "value": value})
+  def format(self, tokensource, _):
+    for ttype, value in tokensource:
+      self.tokens.append({"type": str(ttype), "value": value})
 
-    def result(self):
-        return self.tokens
+  def result(self):
+    return self.tokens
 
 if __name__ == "__main__":
-    for algo in algorithms:
-        for ext in extensions:
-            filename = "./{algo}.bundle/{algo}.{ext}".format(algo=algo, ext=ext)
-            output_filename = "{filename}.json".format(filename=filename)
-            try:
-                with open(filename) as fp:
-                    code = fp.read()
-                lexer = get_lexer_for_filename(filename)
-                formatter = JsonFormatter()
-                highlight(code, lexer, formatter)
-                result = formatter.result()
-                with open(output_filename, "w") as fp:
-                    json.dump({"result": formatter.result()}, fp)
-            except FileNotFoundError:
-                print("Warning: skipping {filename} highlighting (file not found)")
+  for algo in algorithms:
+    for ext in extensions:
+      filename = "./{algo}.bundle/{algo}.{ext}".format(algo=algo, ext=ext)
+      output_filename = "{filename}.json".format(filename=filename)
+      try:
+        with open(filename) as fp:
+          code = fp.read()
+        lexer = get_lexer_for_filename(filename)
+        formatter = JsonFormatter()
+        highlight(code, lexer, formatter)
+        result = formatter.result()
+        with open(output_filename, "w") as fp:
+          json.dump({"result": formatter.result()}, fp)
+      except FileNotFoundError:
+        print("Warning: skipping {filename} highlighting (file not found)")
