f73aa23c95066e2baaad16c5fff51c076cadbfa8
Nicholas Hubbard <hubbardn95@gmail.com>
2022-06-08 09:46:04 -0400

    Insertion Sort: Finish Go implementation

--- Shared/Resources/insertionsort.bundle/insertionsort.go
+++ Shared/Resources/insertionsort.bundle/insertionsort.go
@@ -4,7 +4,19 @@ import (
   "fmt"
 )
 
-/* TODO: Insert Go algorithm implementation here. */
+func sort(arr []int) []int {
+  n := len(arr)
+  for i := 1; i < n; i++ {
+    key := arr[i]
+    j := i - 1
+    for j >= 0 && arr[j] > key {
+      arr[j + 1] = arr[j]
+      j = j - 1
+    }
+    arr[j + 1] = key
+  }
+  return arr
+}
 
 func main() {
   items := []int{35, 95, 74, 71, 72, 30, 96, 53, 9, 0}
