bac97a4b3b475efa84e9717a863b3d358d5820da
Nicholas Hubbard <hubbardn95@gmail.com>
2022-06-08 09:49:36 -0400

    Insertion Sort: Complete Java implementation

--- Shared/Resources/insertionsort.bundle/insertionsort.java
+++ Shared/Resources/insertionsort.bundle/insertionsort.java
@@ -1,8 +1,18 @@
 import java.util.Arrays;
 
-/* TODO: Rename class to match file name (Java specific). */
-public class quicksort {
-  /* TODO: Insert Java algorithm implementation here. */
+public class insertionsort {
+  public static void sort(int[] arr) {
+    int n = arr.length;
+    for (int i = 1; i < n; i++) {
+      int key = arr[i];
+      int j = i - 1;
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+      }
+      arr[j + 1] = key;
+    }
+  }
 
   public static void main(String[] args) {
     int[] items = new int[]{35, 95, 74, 71, 72, 30, 96, 53, 9, 0};
