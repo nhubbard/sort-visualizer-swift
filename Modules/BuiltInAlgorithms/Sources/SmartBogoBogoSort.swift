640879944334e6269910cedf31fa600ae1a1328b
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2026-07-15 18:39:18 -0500

    fix: Reduce potential of uncontrolled array growth

--- Modules/BuiltInAlgorithms/Sources/SmartBogoBogoSort.swift
+++ Modules/BuiltInAlgorithms/Sources/SmartBogoBogoSort.swift
@@ -6,7 +6,7 @@ public struct SmartBogoBogoSort: SortAlgorithm {
     public let metadata = AlgorithmMetadata(
         displayName: "Smart Bogo Bogo Sort",
         category: .impractical,
-        sizeRange: 4...11,
+        sizeRange: 4...8,
         stable: false,
         timeComplexity: ComplexityBounds(best: "O(n)", average: "O(n \\times n!)", worst: "O(n \\times n!)"),
         spaceComplexity: "O(1)",
--- Modules/BuiltInAlgorithms/Sources/StablePermutationSort.swift
+++ Modules/BuiltInAlgorithms/Sources/StablePermutationSort.swift
@@ -6,7 +6,7 @@ public struct StablePermutationSort: SortAlgorithm {
     public let metadata = AlgorithmMetadata(
         displayName: "Stable Permutation Sort",
         category: .exchange,
-        sizeRange: 4...11,
+        sizeRange: 4...8,
         stable: false,
         timeComplexity: ComplexityBounds(best: "O(n)", average: "O(n \\times n!)", worst: "O(n \\times n!)"),
         spaceComplexity: "O(n)",
