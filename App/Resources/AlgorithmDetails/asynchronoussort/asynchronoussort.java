ca292211bfb8b6d84f3012620b76486974f2e831
Nicholas Hubbard <nhubbard@users.noreply.github.com>
2026-07-15 13:42:19 -0500

    task: Finish asynchronous sort

---
+++ App/Resources/AlgorithmDetails/asynchronoussort/asynchronoussort.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class asynchronoussort {
+  public static void sort(int arr[]) {
+    int n = arr.length;
+    int[] ext = new int[n];
+    System.arraycopy(arr, 0, ext, 0, n);
+    int minValue = ext[0];
+    int maxValue = ext[0];
+    for (int k = 0; k < n; k++) {
+      if (ext[k] < minValue) minValue = ext[k];
+      if (ext[k] > maxValue) maxValue = ext[k];
+    }
+    maxValue += 1;
+
+    int cur = minValue;
+    int i = 0;
+    while (i < n) {
+      for (int j = 0; j < n; j++) {
+        if (ext[j] <= cur) {
+          arr[i] = ext[j];
+          ext[j] = maxValue;
+          i += 1;
+        }
+      }
+      cur += 1;
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] array = new int[]{0, 39, 21, 62, 91, 77, 14, 23,
+      90, 69, 51, 81, 68, 83, 32, 56};
+    sort(array);
+    System.out.println(Arrays.toString(array));
+  }
+}
---
+++ App/Resources/AlgorithmDetails/asynchronoussort/asynchronoussort.py
@@ -0,0 +1,19 @@
+def sort(arr):
+    n = len(arr)
+    ext = list(arr)
+    min_value = min(ext)
+    max_value = max(ext) + 1
+
+    cur = min_value
+    i = 0
+    while i < n:
+        for j in range(n):
+            if ext[j] <= cur:
+                arr[i] = ext[j]
+                ext[j] = max_value
+                i += 1
+        cur += 1
+
+array = [0, 39, 21, 62, 91, 77, 14, 23, 90, 69, 51, 81, 68, 83, 32, 56]
+sort(array)
+print(array)
